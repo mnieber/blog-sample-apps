2a3814ab79fefa628cf19e2b308d4a5fc058fad6
Maarten Nieber <hallomaarten@yahoo.com>
2023-09-18 15:10:10 +0200

    Use getBestRouteMatch

--- navHandler-sample/src/routes/utils/getRouteMatch.ts
+++ navHandler-sample/src/routes/utils/getRouteMatch.ts
@@ -1,6 +1,6 @@
 import { navigation } from '/src/routes/history';
 import { routeTable } from '/src/routes/routeTable';
-import { getBestRouteMatch } from '/src/routes/utils/getRouteMatches';
+import { getBestRouteMatch } from '/src/routes/utils/getBestRouteMatch';
 
 export const getRouteMatch = () => {
   return getBestRouteMatch(
